48e27daf3e2920b954b54adf4e2ab4954961c192
Gabriel Camatta Zanotelli <55805329+Zanotelli@users.noreply.github.com>
2023-04-12 22:43:13 -0300

    TP1

--- TP1/src/main.c
+++ TP1/src/main.c
@@ -1,5 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LINES 20
 
 int main(int argc, char ** argv){
 
+    FILE *fp;
+    char commands[MAX_LINES];
+
+
+    fp = fopen("../input.txt", "r"); 
+
+
+    if(fp == NULL) {
+        printf("Error opening file.\n");
+        return -1;
+    }
+
+    while(fgets(commands, MAX_LINES, fp) != NULL) {
+        printf("%s", commands); // do something with each line in the file
+    }
+
+    fclose(fp);
     return 0;
 }
